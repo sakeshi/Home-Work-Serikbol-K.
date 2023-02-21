fa4be8d5f0ef06b680a840111f4d66e6111e7283
sakeshi <125754419+sakeshi@users.noreply.github.com>
2023-02-21 21:37:56 +0600

    Add files via upload

---
+++ population.py
@@ -0,0 +1,20 @@
+
+num1 = int(input("Start size: "))
+num2 = int(input("End size: "))
+total =0
+n=0
+k= num1
+
+while num1<9:
+    total +=num1
+    num1 = int(input("Start size: "))
+
+while num2<num1:
+    total +=num2
+    num2 = int(input("End size: "))
+
+while k<num2:s
+   k = k + (k // 3) - (k // 4)
+   n= n+1
+else:
+   print("Years: ",n-1)
